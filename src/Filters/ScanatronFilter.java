9d32d9a9c859d40bc10b08f268ad526d579e8b06
Meah <meah.set@gmail.com>
2024-02-13 08:42:53 -0800

    various changes

--- src/Filters/ScanatronFilter.java
+++ src/Filters/ScanatronFilter.java
@@ -3,7 +3,6 @@ package Filters;
 import Interfaces.PixelFilter;
 import core.DImage;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ScanatronFilter implements PixelFilter {
@@ -40,7 +39,7 @@ public class ScanatronFilter implements PixelFilter {
         return answers;
     }
     public String getAnswer(short[][] grid, int x1, int y){
-        double[] bubbleBlackValue = getBubbleBlackValue(grid, x1, y);
+        double[] bubbleBlackValue = getBubbleAverage(grid, x1, y);
         double MAX = 0;
         int MAXINDEX = 0;
         for (int i = 0; i < bubbleBlackValue.length; i++) {
@@ -50,21 +49,25 @@ public class ScanatronFilter implements PixelFilter {
         }
         return s.substring(MAXINDEX, MAXINDEX + 1);
     }
-    public double[] getBubbleBlackValue(short[][] grid, int x1, int y){
-        double[] bubbleBlackValue = new double[bubbleRowCount];
+    public double[] getBubbleAverage(short[][] grid, int x1, int y){
+        double[] bubbleAverage = new double[bubbleRowCount];
         for (int i = 0; i < bubbleRowCount; i ++) {
             int x = x1 + (i * bubbleHoriSpacing);
-            bubbleBlackValue[i] = getBubbleBlack(grid, x, y, 20);
+            bubbleAverage[i] = getBubbleAverage(grid, x, y, 20);
         }
-        return bubbleBlackValue;
+        return bubbleAverage;
     }
-    public double getBubbleBlack(short[][] grid, int x1, int y1, int bubbleS){
+    public double getBubbleAverage(short[][] grid, int x1, int y1, int bubbleS){
+        int totalBubblePixels = 0;
+        int blackPixels = 0;
         for (int x = x1; x < x + bubbleS; x++) {
             for (int y = y1; y < y + bubbleS; y++) {
+                totalBubblePixels++;
                 if(grid[x][y] == 0){
-
+                    blackPixels++;
                 }
             }
         }
+        return (double) totalBubblePixels/blackPixels;
     }
 }
